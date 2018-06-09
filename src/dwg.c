0d2d6f9195c1582bd20d7994bbd3741b8688e8f6
Reini Urban <rurban@cpan.org>
2018-06-09 18:07:31 +0200

    add in_json with MIT jsmn WIP
    as a submodule

--- src/dwg.c
+++ src/dwg.c
@@ -52,8 +52,8 @@ static bool env_var_checked_p;
 Dwg_Object *
 dwg_resolve_handle_silent(const Dwg_Data * dwg, const BITCODE_BL absref);
 
-static int dat_read_file (Bit_Chain *restrict dat, FILE *restrict fp,
-                          const char *restrict filename)
+int dat_read_file (Bit_Chain *restrict dat, FILE *restrict fp,
+                   const char *restrict filename)
 {
   size_t size;
   dat->chain = (unsigned char *) calloc(1, dat->size);
@@ -78,7 +78,7 @@ static int dat_read_file (Bit_Chain *restrict dat, FILE *restrict fp,
   return 0;
 }
 
-static int dat_read_stream (Bit_Chain *restrict dat, FILE *restrict fp)
+int dat_read_stream (Bit_Chain *restrict dat, FILE *restrict fp)
 {
   size_t size = 0;
 
