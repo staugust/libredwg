125abdac7de1103bc5c9c37c2c244546ea76db49
Reini Urban <rurban@cpan.org>
2019-01-23 11:21:18 +0100

    dynapi: check countfields
    of arrays

--- src/gen-dynapi.pl
+++ src/gen-dynapi.pl
@@ -676,10 +676,10 @@ EOF
     $type $var;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value($lname, "$name", "$countfield", &count, NULL) &&
-        dwg_dynapi_entity_value($lname, "$name", "$var", &$svar, NULL)
+        dwg_dynapi_entity_values($lname, "$name", "$var", &$svar, count)
 EOF
         if ($type !~ /\*\*/) {
-          print $fh "        && $svar == $lname->$svar";
+          print $fh "        && (!count || !memcmp(&$svar, &$lname->$svar, count * sizeof($lname->$svar)))\n";
         }
         print $fh ")\n";
         print $fh <<"EOF";
@@ -937,6 +937,8 @@ dwg_dynapi_entity_values(void *restrict _obj, const char *restrict name,
 {
   if (!_obj)
     return false;
+  if (!count)
+    return true;
   {
     int error;
     // check the object type
@@ -957,14 +959,12 @@ dwg_dynapi_entity_values(void *restrict _obj, const char *restrict name,
           LOG_ERROR("%s: Invalid %s field %s", __FUNCTION__, name, fieldname);
           return false;
         }
-      //TODO: if (f->malloc)
-      //  which size? if text strcpy. if TU wcscpy. if struct num_fieldname * f->size
       memcpy(out, &((char*)_obj)[f->offset], count * f->size);
       return true;
     }
   }
 }
- 
+
 EXPORT bool
 dwg_dynapi_header_value(const Dwg_Data *restrict dwg, const char *restrict fieldname,
                         void *restrict out, Dwg_DYNAPI_field *restrict fp)
@@ -1050,6 +1050,8 @@ dwg_dynapi_entity_set_value(void *restrict _obj, const char *restrict name,
 {
   if (!_obj)
     return false;
+  if (!count)
+    return true;
   {
     int error;
     const Dwg_Object* obj = dwg_obj_generic_to_object(_obj, &error);
